830259f4fb73c27fb17f7dac1053c3cc08b7de04
Abhishek <nintendo.spy123@gmail.com>
2020-07-24 10:40:14 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,36 @@
+var n = 1;
+var count = 0;
+function a()
+{
+    if (n == 1){
+        var seconds = 30;
+        var countdown = setInterval(function() {
+            seconds--;
+            document.getElementById("countdown").textContent = "You have " + seconds + " seconds left, hurry!";
+            if (seconds <= 0) clearInterval(countdown);
+            if(seconds == 0){
+                if (count > 120){
+                    alert("Wow! You are pretty fast!")
+                    alert("Thanks for playing the click test, your score was " + count, " Share it!");
+                }
+                else if (count > 100 && count < 120)
+                {
+                    alert("Not bad, but you could improve.");
+                    alert("Thanks for playing the click test, your score was " + count, " Share it!");
+                }
+                else{
+                    alert("You can do better, try again by refreshing the page.");
+                    alert("Thanks for playing the click test, your score was " + count, + " Share it!");
+                }
+        }
+        }, 1000);
+        n--;
+    }
+    count++;
+    /*
+    We are only going to run the timer function when
+    n is clicked the first time, so it doesn't run
+    for every single click.
+    */
+    document.getElementById("count").textContent = count + " clicks";
+}
